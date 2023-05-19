dc32cd7bdb1614600ef937718019db53f390d95a
Ali Jan <alijan061333@gmail.com>
2023-05-19 15:32:40 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+document.querySelector('.cross').style.display='none';
+document.querySelector('.ham').addEventListener("click",()=> {
+    document.querySelector('.left-side').classList.toggle('sidebargo')
+})
+document.getElementById('kim').style.display='none';
+
+
+// document.getElementsByTagName('button').addEventListener("click",() => {
+//     document.
+// })
+ 
